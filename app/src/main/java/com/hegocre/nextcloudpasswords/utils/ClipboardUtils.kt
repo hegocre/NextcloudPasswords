30c75fdd3b1b16e7f8070c82a35d6e0187053505
hegocre <15657088+hegocre@users.noreply.github.com>
2023-08-08 00:44:33 +0200

    Mark copied passwords as sensitive

--- app/src/main/java/com/hegocre/nextcloudpasswords/utils/ClipboardUtils.kt
+++ app/src/main/java/com/hegocre/nextcloudpasswords/utils/ClipboardUtils.kt
@@ -1,11 +1,19 @@
 package com.hegocre.nextcloudpasswords.utils
 
 import android.content.ClipData
+import android.content.ClipDescription
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.Build
+import android.os.PersistableBundle
 
 fun Context.copyToClipboard(value: String) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    val clip = ClipData.newPlainText("text", value)
+    val clip = ClipData.newPlainText("text/plain", value)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val extras = PersistableBundle()
+        extras.putBoolean(ClipDescription.EXTRA_IS_SENSITIVE, isSensitive)
+        clip.description.extras = extras
+    }
     clipboard.setPrimaryClip(clip)
 }
