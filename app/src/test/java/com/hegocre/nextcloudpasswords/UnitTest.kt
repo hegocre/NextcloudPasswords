604cf8f5281535fa633a78a1c2590e6e23f4eef7
hegocre <15657088+hegocre@users.noreply.github.com>
2023-08-12 18:07:52 +0200

    Add SHA1 test

--- app/src/test/java/com/hegocre/nextcloudpasswords/UnitTest.kt
+++ app/src/test/java/com/hegocre/nextcloudpasswords/UnitTest.kt
@@ -1,12 +1,20 @@
 package com.hegocre.nextcloudpasswords
 
+import com.hegocre.nextcloudpasswords.utils.sha1Hash
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Unit which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 
 class UnitTest {
-
+    @Test
+    fun sha1Test() {
+        val hash = "test_string".sha1Hash()
+        assertEquals(hash, "c58efadcf9f6b303e44e4a62dd984bbc8cae6e99")
+    }
 }
