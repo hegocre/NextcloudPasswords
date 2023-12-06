f53709275bd07a43a0a08166e0738bf77f2587f1
hegocre <15657088+hegocre@users.noreply.github.com>
2023-12-07 00:30:20 +0100

    Fix settings not scrollable

--- app/src/main/java/com/hegocre/nextcloudpasswords/ui/components/NCPSettings.kt
+++ app/src/main/java/com/hegocre/nextcloudpasswords/ui/components/NCPSettings.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
