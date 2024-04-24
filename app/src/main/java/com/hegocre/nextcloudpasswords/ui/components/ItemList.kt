4c16dc45584db61138f7ce7b5b8143be4f898900
hegocre <15657088+hegocre@users.noreply.github.com>
2024-04-24 15:57:29 +0200

    Implement scrollbars

--- app/src/main/java/com/hegocre/nextcloudpasswords/ui/components/ItemList.kt
+++ app/src/main/java/com/hegocre/nextcloudpasswords/ui/components/ItemList.kt
@@ -1,5 +1,7 @@
 package com.hegocre.nextcloudpasswords.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
@@ -7,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Folder
@@ -18,15 +22,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.hegocre.nextcloudpasswords.R
 import com.hegocre.nextcloudpasswords.data.folder.Folder
@@ -56,8 +67,23 @@ fun MixedLazyColumn(
     val context = LocalContext.current
     val shouldShowIcon by PreferencesManager.getInstance(context).getShowIcons()
         .collectAsState(initial = false, context = Dispatchers.IO)
+    val listState = rememberLazyListState()
+    val knobRatio by remember {
+        derivedStateOf { (10f / ((passwords?.size ?: 0) + (folders?.size ?: 0))).coerceIn(0f, 1f) }
+    }
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .then(
+                if (knobRatio == 1f) Modifier else Modifier.scrollbar(
+                    state = listState,
+                    horizontal = false,
+                    visibleAlpha = 0.5f,
+                    fixedKnobRatio = knobRatio,
+                    knobCornerRadius = 0.dp
+                )
+            ),
+        state = listState
     ) {
         folders?.let {
             items(items = it, key = { folder -> folder.id }) { folder ->
@@ -180,6 +206,142 @@ fun FolderRow(
                 }
             )
     )
+}
+
+// Scrollbars added from https://stackoverflow.com/questions/66341823/jetpack-compose-scrollbars/71932181#71932181
+@Composable
+fun Modifier.scrollbar(
+    state: LazyListState,
+    horizontal: Boolean,
+    alignEnd: Boolean = true,
+    thickness: Dp = 4.dp,
+    fixedKnobRatio: Float? = null,
+    knobCornerRadius: Dp = 4.dp,
+    trackCornerRadius: Dp = 2.dp,
+    knobColor: Color = MaterialTheme.colorScheme.onSurface,
+    padding: Dp = 0.dp,
+    visibleAlpha: Float = 1f,
+    hiddenAlpha: Float = 0f,
+    fadeInAnimationDurationMs: Int = 150,
+    fadeOutAnimationDurationMs: Int = 500,
+    fadeOutAnimationDelayMs: Int = 1000,
+): Modifier {
+    check(thickness > 0.dp) { "Thickness must be a positive integer." }
+    check(fixedKnobRatio == null || fixedKnobRatio < 1f) {
+        "A fixed knob ratio must be smaller than 1."
+    }
+    check(knobCornerRadius >= 0.dp) { "Knob corner radius must be greater than or equal to 0." }
+    check(trackCornerRadius >= 0.dp) { "Track corner radius must be greater than or equal to 0." }
+    check(hiddenAlpha <= visibleAlpha) { "Hidden alpha cannot be greater than visible alpha." }
+    check(fadeInAnimationDurationMs >= 0) {
+        "Fade in animation duration must be greater than or equal to 0."
+    }
+    check(fadeOutAnimationDurationMs >= 0) {
+        "Fade out animation duration must be greater than or equal to 0."
+    }
+    check(fadeOutAnimationDelayMs >= 0) {
+        "Fade out animation delay must be greater than or equal to 0."
+    }
+
+    val targetAlpha =
+        if (state.isScrollInProgress) {
+            visibleAlpha
+        } else {
+            hiddenAlpha
+        }
+    val animationDurationMs =
+        if (state.isScrollInProgress) {
+            fadeInAnimationDurationMs
+        } else {
+            fadeOutAnimationDurationMs
+        }
+    val animationDelayMs =
+        if (state.isScrollInProgress) {
+            0
+        } else {
+            fadeOutAnimationDelayMs
+        }
+
+    val alpha by
+    animateFloatAsState(
+        targetValue = targetAlpha,
+        animationSpec =
+        tween(delayMillis = animationDelayMs, durationMillis = animationDurationMs),
+        label = "alpha"
+    )
+
+    return drawWithContent {
+        drawContent()
+
+        state.layoutInfo.visibleItemsInfo.firstOrNull()?.let { firstVisibleItem ->
+            if (state.isScrollInProgress || alpha > 0f) {
+                // Size of the viewport, the entire size of the scrollable composable we are decorating with
+                // this scrollbar.
+                val viewportSize =
+                    if (horizontal) {
+                        size.width
+                    } else {
+                        size.height
+                    } - padding.toPx() * 2
+
+                // The size of the first visible item. We use this to estimate how many items can fit in the
+                // viewport. Of course, this works perfectly when all items have the same size. When they
+                // don't, the scrollbar knob size will grow and shrink as we scroll.
+                val firstItemSize = firstVisibleItem.size
+
+                // The *estimated* size of the entire scrollable composable, as if it's all on screen at
+                // once. It is estimated because it's possible that the size of the first visible item does
+                // not represent the size of other items. This will cause the scrollbar knob size to grow
+                // and shrink as we scroll, if the item sizes are not uniform.
+                val estimatedFullListSize = firstItemSize * state.layoutInfo.totalItemsCount
+
+                // The difference in position between the first pixels visible in our viewport as we scroll
+                // and the top of the fully-populated scrollable composable, if it were to show all the
+                // items at once. At first, the value is 0 since we start all the way to the top (or start
+                // edge). As we scroll down (or towards the end), this number will grow.
+                val viewportOffsetInFullListSpace =
+                    state.firstVisibleItemIndex * firstItemSize + state.firstVisibleItemScrollOffset
+
+                // Where we should render the knob in our composable.
+                val knobPosition =
+                    (viewportSize / estimatedFullListSize) * viewportOffsetInFullListSpace + padding.toPx()
+                // How large should the knob be.
+                val knobSize =
+                    fixedKnobRatio?.let { it * viewportSize }
+                        ?: ((viewportSize * viewportSize) / estimatedFullListSize)
+
+                // Draw the knob
+                drawRoundRect(
+                    color = knobColor,
+                    topLeft =
+                    when {
+                        // When the scrollbar is horizontal and aligned to the bottom:
+                        horizontal && alignEnd -> Offset(
+                            knobPosition,
+                            size.height - thickness.toPx()
+                        )
+                        // When the scrollbar is horizontal and aligned to the top:
+                        horizontal && !alignEnd -> Offset(knobPosition, 0f)
+                        // When the scrollbar is vertical and aligned to the end:
+                        alignEnd -> Offset(size.width - thickness.toPx(), knobPosition)
+                        // When the scrollbar is vertical and aligned to the start:
+                        else -> Offset(0f, knobPosition)
+                    },
+                    size =
+                    if (horizontal) {
+                        Size(knobSize, thickness.toPx())
+                    } else {
+                        Size(thickness.toPx(), knobSize)
+                    },
+                    alpha = alpha,
+                    cornerRadius = CornerRadius(
+                        x = knobCornerRadius.toPx(),
+                        y = knobCornerRadius.toPx()
+                    ),
+                )
+            }
+        }
+    }
 }
 
 @Preview
