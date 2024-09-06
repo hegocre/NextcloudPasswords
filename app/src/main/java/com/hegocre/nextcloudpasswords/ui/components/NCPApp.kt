763dc570224d4a60cc2abf84bfac737d34eb0af5
hegocre <15657088+hegocre@users.noreply.github.com>
2024-09-07 01:06:44 +0200

    Improve bottom sheet animations

--- app/src/main/java/com/hegocre/nextcloudpasswords/ui/components/NCPApp.kt
+++ app/src/main/java/com/hegocre/nextcloudpasswords/ui/components/NCPApp.kt
@@ -74,7 +74,8 @@ fun NextcloudPasswordsApp(
         backstackEntry.value?.destination?.route
     )
 
-    val modalSheetState = rememberModalBottomSheetState()
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    val modalSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
     val needsMasterPassword by passwordsViewModel.needsMasterPassword.collectAsState()
     val masterPasswordInvalid by passwordsViewModel.masterPasswordInvalid.collectAsState()
@@ -227,9 +228,7 @@ fun NextcloudPasswordsApp(
                 openPasswordDetails = { password ->
                     passwordsViewModel.setVisiblePassword(password)
                     keyboardController?.hide()
-                    coroutineScope.launch {
-                        modalSheetState.show()
-                    }
+                    openBottomSheet = true
                 },
                 replyAutofill = replyAutofill,
                 searchVisibility = searchExpanded,
@@ -283,14 +282,11 @@ fun NextcloudPasswordsApp(
                 )
             }
 
-            if (modalSheetState.isVisible) {
+            if (openBottomSheet) {
                 ModalBottomSheet(
-                    onDismissRequest = {
-                        coroutineScope.launch {
-                            modalSheetState.hide()
-                        }
-                    },
-                    contentWindowInsets = { WindowInsets.navigationBars }
+                    onDismissRequest = { openBottomSheet = false },
+                    contentWindowInsets = { WindowInsets.navigationBars },
+                    sheetState = modalSheetState
                 ) {
                     PasswordItem(
                         password = passwordsViewModel.visiblePassword.value,
@@ -298,6 +294,10 @@ fun NextcloudPasswordsApp(
                             {
                                 coroutineScope.launch {
                                     modalSheetState.hide()
+                                }.invokeOnCompletion {
+                                    if (!modalSheetState.isVisible) {
+                                        openBottomSheet = false
+                                    }
                                 }
                                 navController.navigate("${NCPScreen.PasswordEdit.name}/${passwordsViewModel.visiblePassword.value?.id ?: "none"}")
                             }
