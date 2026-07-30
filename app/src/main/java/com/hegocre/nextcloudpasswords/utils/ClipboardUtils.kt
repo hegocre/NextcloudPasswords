78cc7cbbd82ef9eec358eb05556e87419320d43a
hegocre <15657088+hegocre@users.noreply.github.com>
2026-07-30 23:58:12 +0200

    Mark passwords as sensitive on older android versions

--- app/src/main/java/com/hegocre/nextcloudpasswords/utils/ClipboardUtils.kt
+++ app/src/main/java/com/hegocre/nextcloudpasswords/utils/ClipboardUtils.kt
@@ -9,11 +9,13 @@ import android.os.PersistableBundle
 
 fun Context.copyToClipboard(value: String, isSensitive: Boolean = false) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    val clip = ClipData.newPlainText("text/plain", value)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        val extras = PersistableBundle()
-        extras.putBoolean(ClipDescription.EXTRA_IS_SENSITIVE, isSensitive)
-        clip.description.extras = extras
+    val clip = ClipData.newPlainText(if (isSensitive) "password" else "text/plain",
+        value)
+    clip.description.extras = PersistableBundle().apply {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            putBoolean(ClipDescription.EXTRA_IS_SENSITIVE, isSensitive)
+        else
+            putBoolean("android.content.extra.IS_SENSITIVE", isSensitive)
     }
     clipboard.setPrimaryClip(clip)
 }
